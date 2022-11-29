cbecd6d312f1664e6a70f8becbd7bfe5af5efc06
linyinjun <c0a21141e4@edu.teu.ac.jp>
2022-11-29 14:10:19 +0900

    テスト

---
+++ ex02/sanpuru.py
@@ -0,0 +1,32 @@
+import tkinter as tk 
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
+
+
+
+root = tk.Tk()
+root.title("game")
+root.geometry("500x200")
+
+label = tk.Label(root,
+                text="ラベルを書いてみた件",
+                font=("",20),
+                )
+label.pack()
+
+button = tk.Button(root,text="押すな")
+button.bind("<1>",button_click)
+button.pack()
+
+entry = tk.Entry(width=30)
+entry.insert(tk.END,"fugapiyo")
+entry.pack()
+
+
+tkm.showerror("タイトル","メッセージ")
+
+root.mainloop()
