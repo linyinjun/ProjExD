87bba7383783e43dfd5c05d0d7a1771a48679a84
linyinjun <c0a21141e4@edu.teu.ac.jp>
2022-11-29 17:17:34 +0900

    更新

--- ex02/calc.py
+++ ex02/calc.py
@@ -14,31 +14,59 @@ def button_click(event):
     #tkm.showinfo(txt,f"[{txt}]ボタンがクリックされました")
         #6
         entry.insert(tk.END, i)
-
+    if i =="C":
+         entry.delete(0,tk.END)
+         entry.insert(tk.END)
+    
 #練習1
 root = tk.Tk()
 root.title("calc")
-root.geometry("300x500")
+root.geometry("450x500")
 
 #練習4
 entry = tk.Entry(root, justify="right",width=10,font=("",40))
 entry.grid(row=0, column=0, columnspan=3)
 
 #練習2
-r, c=1, 0 
+r, c=2, 2 
 for i in range(9,-1,-1):
-    button = tk.Button(root, text=f"{i}", width=4, height=2, font=("",30))
-    button.grid(row=r,column=c)
+    if i == 0:
+        button = tk.Button(root, text=f"{i}", width=4, height=2, font=("",30))
+        button.grid(row=5,column=0)
+    else:
+        button = tk.Button(root, text=f"{i}", width=4, height=2, font=("",30))
+        button.grid(row=r,column=c)
+    
     button.bind("<1>",button_click)
-    c += 1
-    if c%3 == 0:
+    c -= 1
+    if c == -1:
         r += 1
-        c = 0
+        c = 2
+button = tk.Button(root, text=f"00", width=4, height=2, font=("",30))
+button.grid(row=5,column=1)
+button.bind("<1>",button_click)
 
 #5
-operatos=["+","="]
+operatos=["/", "*", "-", "+", "=","."]
+r, c=1, 3
 for ope in operatos:
-    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("",30))
+    if ope == ".":
+        button = tk.Button(root, text=f".", width=4, height=2, font=("",30))
+        button.grid(row=5,column=2)     
+    else:
+         button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("",30))
+         button.grid(row=r,column=c)
+
+    
+    button.bind("<1>",button_click)
+    if c%3== 0:
+        r += 1
+
+    
+symbols=["C","AC","%"]
+r, c=1, 0
+for sym in symbols:
+    button = tk.Button(root, text=f"{sym}", width=4, height=2, font=("",30))
     button.grid(row=r,column=c)
     button.bind("<1>",button_click)
     c += 1
