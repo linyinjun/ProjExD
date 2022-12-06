7cf8947009620ba659148f56d8cedce8d2d8ea5f
linyinjun <c0a21141e4@edu.teu.ac.jp>
2022-12-06 17:12:22 +0900

    迷路追加機能

--- ex3/mazu.py
+++ ex3/mazu.py
@@ -1,14 +1,41 @@
+from random import randint
 import tkinter as tk
 import maze_maker as mm
+import tkinter.messagebox as tkm
+import sample as sp
 
+global ct
+ct = True
+
+def count_up():
+    global tmr, jid 
+    label["text"] = tmr
+    tmr = tmr+1
+    jid = root.after(1000, count_up)
+
+def key_down(event):
+    global jid
+    print(jid)
+    if jid is not None: 
+        # カウントアップ中にキーが押されたら
+        # カウントアップ中でないときは，jid is None
+        root.after_cancel(jid)
+        print("cancel")
+        jid = None
+    
 def key_down(event):
     global key
     key = event.keysym
 
 
 def key_up(event):
-    global key
+    global key,ct
     key = ""
+    if cx == 1350 and cy == 750 and ct == True:
+        tkm.showwarning(root," ゴール")
+        cl = None
+                
+
 
 
 def main_proc():
@@ -33,15 +60,32 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
 
+    label = tk.Label(root, font=("",80))
+    label.pack()
+
+    tmr = 0
+    jid = None
+
     maze_lst = mm.make_maze(15, 9)
     # print(maze_lst)
     mm.show_maze(canvas, maze_lst)
+
+    sx, sy =1*100+50,1*100+50
+    z = tk.PhotoImage(file="fig/0.png")
+    canvas.create_image(sx, sy, image=z)
+
+    gx, gy = 1350 ,750
+    zu = tk.PhotoImage(file="fig/6.png")
+    canvas.create_image(gx, gy, image=zu)
 
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
     tori = tk.PhotoImage(file="fig/8.png")
     canvas.create_image(cx, cy, image=tori, tag="kokaton")
+    
     key = ""
+    tmr = 0
+    jid = None
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     main_proc()
