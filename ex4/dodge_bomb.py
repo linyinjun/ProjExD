e01e6f792d0a4759135044dc4a54e0c942bd2d77
linyinjun <c0a21141e4@edu.teu.ac.jp>
2022-12-13 17:38:39 +0900

    練習

--- ex4/dodge_bomb.py
+++ ex4/dodge_bomb.py
@@ -1,31 +1,31 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
 import pygame as pg
 import random
 import sys
-
 
 def check_bound(obj_rct, scr_rct):
     # 第1引数：こうかとんrectまたは爆弾rect
     # 第2引数：スクリーンrect
     # 範囲内：+1／範囲外：-1
     yoko, tate = +1, +1
-    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
-        yoko = -1
-    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
-        tate = -1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right : yoko = -1.4
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom : tate = -1.4
     return yoko, tate
 
-
 def main():
+    global tmr,tms
     clock =pg.time.Clock()
     # 練習１
     pg.display.set_caption("逃げろ！こうかとん")
-    scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_sfc = pg.display.set_mode((1400, 800))
     scrn_rct = scrn_sfc.get_rect()
-    pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
+    pgbg_sfc = pg.image.load("/Users/linyinjun/Desktop/学校/２年/後期/演習テーマD/ProjExD2022/ProjExD/fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
+    scrn_sfc.blit(pgbg_sfc,pgbg_rct)
 
     # 練習３
-    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.image.load("/Users/linyinjun/Desktop/学校/２年/後期/演習テーマD/ProjExD2022/ProjExD/fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
@@ -33,9 +33,11 @@ def main():
     scrn_sfc.blit(tori_sfc, tori_rct) 
 
     # 練習５
-    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
-    bomb_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_sfc = pg.image.load("/Users/linyinjun/Desktop/学校/２年/後期/演習テーマD/ProjExD2022/ProjExD/fig/13.png")
+    #bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
+    #bomb_sfc.set_colorkey((0, 0, 0))
+    #pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_sfc = pg.transform.rotozoom(bomb_sfc, 0, 3.0)
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx = random.randint(0, scrn_rct.width)
     bomb_rct.centery = random.randint(0, scrn_rct.height)
@@ -52,24 +54,16 @@ def main():
 
         # 練習4
         key_dct = pg.key.get_pressed() # 辞書型
-        if key_dct[pg.K_UP]:
-            tori_rct.centery -= 1
-        if key_dct[pg.K_DOWN]:
-            tori_rct.centery += 1
-        if key_dct[pg.K_LEFT]:
-            tori_rct.centerx -= 1
-        if key_dct[pg.K_RIGHT]:
-            tori_rct.centerx += 1
+        if key_dct[pg.K_UP]   : tori_rct.centery -= 10
+        if key_dct[pg.K_DOWN] : tori_rct.centery += 10
+        if key_dct[pg.K_LEFT] : tori_rct.centerx -= 10
+        if key_dct[pg.K_RIGHT]: tori_rct.centerx += 10
         if check_bound(tori_rct, scrn_rct) != (+1, +1):
             # どこかしらはみ出ていたら
-            if key_dct[pg.K_UP]:
-                tori_rct.centery += 1
-            if key_dct[pg.K_DOWN]:
-                tori_rct.centery -= 1
-            if key_dct[pg.K_LEFT]:
-                tori_rct.centerx += 1
-            if key_dct[pg.K_RIGHT]:
-                tori_rct.centerx -= 1            
+            if key_dct[pg.K_UP]   : tori_rct.centery += 50
+            if key_dct[pg.K_DOWN] : tori_rct.centery -= 50
+            if key_dct[pg.K_LEFT] : tori_rct.centerx += 50
+            if key_dct[pg.K_RIGHT]: tori_rct.centerx -= 50           
         scrn_sfc.blit(tori_sfc, tori_rct) 
 
         # 練習６
@@ -90,5 +84,6 @@ def main():
 if __name__ == "__main__":
     pg.init()
     main()
+    tkm.showerror(tkm.showerror("負けた"))
     pg.quit()
     sys.exit()
